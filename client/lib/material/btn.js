aaf9cd00be72c16b2dd513e72b8ad2001ae047ba
DerrickJames <dmushangi@gmail.com>
2015-03-23 06:11:59 +0300

    Added the Daemonite material implemetation

---
+++ client/lib/material/btn.js
@@ -0,0 +1,10 @@
+// btn active
+	$(document).on('mousedown touchstart', '.btn', function() {
+		var $this = $(this);
+		if (!$this.hasClass('btn-active')) {
+			$this.addClass('btn-active');
+			setTimeout(function() {
+				$this.removeClass('btn-active');
+			}, 450);
+		}
+	});
